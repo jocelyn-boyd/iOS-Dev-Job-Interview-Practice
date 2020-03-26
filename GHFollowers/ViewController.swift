d3a9771d377dbb634f2c97230973d462c6a323b6
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-03-26 19:12:31 -0400

    initial project setup - no storyboard

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Jocelyn Boyd on 3/26/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
