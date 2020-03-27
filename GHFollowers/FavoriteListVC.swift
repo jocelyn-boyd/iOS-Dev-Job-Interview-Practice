9e683e6f07c6166733ebe168540bbc231524574b
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-03-26 22:51:22 -0400

    App Navigation Setup - UITabBarController & UINavigationController

---
+++ GHFollowers/FavoriteListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteListVC.swift
+//  GHFollowers
+//
+//  Created by Jocelyn Boyd on 3/26/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Jocelyn Boyd on 3/26/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
