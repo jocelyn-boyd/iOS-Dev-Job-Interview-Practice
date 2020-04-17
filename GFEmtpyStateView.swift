b80bb1f1b5dd51838677a80b83ad311d4fb51328
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-04-17 12:53:11 -0400

    Implemented Loading View

---
+++ GFEmtpyStateView.swift
@@ -0,0 +1,21 @@
+//
+//  GFEmtpyStateView.swift
+//  GHFollowers
+//
+//  Created by Jocelyn Boyd on 4/17/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class GFEmtpyStateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
