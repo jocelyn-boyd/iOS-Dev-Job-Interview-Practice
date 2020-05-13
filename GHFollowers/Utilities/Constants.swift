adedffd9308c49820d6665cb2e6d2ff105b38b4a
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-05-12 22:32:47 -0400

    Child View Controller - UserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Jocelyn Boyd on 5/12/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+  static let location = "mappin.and.ellipse"
+}
